da5ac6592789a7b3aa5cbce00aef8d7ef631736e
FarhinS <farhin.shekh@skyttus.com>
2023-07-23 20:21:57 +0530

    Server url.

--- src/pages/authentication/auth-forms/AuthLogin.js
+++ src/pages/authentication/auth-forms/AuthLogin.js
@@ -49,7 +49,7 @@ const AuthLogin = () => {
         // navigate('/')
         axios
             .post(
-                'http://127.0.0.1:8000/api/sign-in/',
+                process.env.REACT_APP_API_BASE_URL,
                 // {
                 //     "username": "userOne",
                 //     "password": "User@123"
